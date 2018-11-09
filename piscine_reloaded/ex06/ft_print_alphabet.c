3d45006d31c18b1eb33db7adbb616018af120cbf
vphongph <vphongph@student.42.fr>
2018-11-09 16:16:41 +0100

    update

--- piscine_reloaded/ex06/ft_print_alphabet.c
+++ piscine_reloaded/ex06/ft_print_alphabet.c
@@ -1,11 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
-/*   Updated: 2018/02/26 23:58:36 by vphongph         ###   ########.fr       */
+/*   Updated: 2018/11/09 01:53:37 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void);
+
+void	ft_print_alphabet()
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
+
+int	main()
+{
+	ft_print_alphabet();
+
+	return(0);
+}
