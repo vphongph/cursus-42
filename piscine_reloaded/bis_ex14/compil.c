d14af140537a2fcfcce51e40e1f5f3f7bc0a6352
vphongph <vphongph@student.42.fr>
2018-11-11 03:09:06 +0100

    push ex14 sqrt

---
+++ piscine_reloaded/bis_ex14/compil.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compil.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
+/*   Updated: 2018/11/11 03:07:00 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print(unsigned int a)
+{
+	if (a >= 10)
+		ft_print(a / 10);
+	ft_putchar(a % 10 + '0');
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int a;
+
+	a = (unsigned int)nb;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		a = (unsigned int)(-nb);
+	}
+	ft_print(a);
+}
+
+//printf("blabla = \n%s", arg);
+
+int		ft_sqrt(int nb);
+
+int		main(void)
+// int	main(int ac, char **av)
+{
+
+	int nb = 2147483647;
+
+	printf("\nnb = %i, sqrt = %i", nb, ft_sqrt(nb));
+
+	// if (ac != 2)
+		// return 0;
+	// ft_putnbr(atoi(av[1]));
+	// ft_putchar('\n');
+	return (0);
+}
---
+++ piscine_reloaded/ex13/ft_recursive_factorial.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/11 01:37:05 by vphongph          #+#    #+#             */
+/*   Updated: 2018/11/11 02:05:11 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_recursive_factorial(int nb)
+{
+	if (nb < 0 || nb > 12)
+		return (0);
+	if (nb == 0 || nb == 1)
+		return (1);
+	if (nb > 2)
+		nb = nb * ft_recursive_factorial(nb - 1);
+	return (nb);
+}
