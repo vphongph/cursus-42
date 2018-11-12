ec7c80ffe4e732b7dac8e5a0ede007edbee29f34
vphongph <vphongph@student.42.fr>
2018-11-12 23:04:54 +0100

    push ex20 str dup

---
+++ piscine_reloaded/bis_ex20/ft_strdup.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/12 22:11:50 by vphongph          #+#    #+#             */
+/*   Updated: 2018/11/12 23:02:01 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = -1;
+	if (!(dest = (char*)malloc((ft_strlen(src) + 1) * sizeof(*dest))))
+		return (NULL);
+	while (src[++i])
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
