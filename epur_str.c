e302bcd42eb1804c0c2e5a82ddcacf4c035f6c94
vphongph <vphongph@student.42.fr>
2018-11-13 16:26:11 +0100

    update

---
+++ epur_str.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   epur_str.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/13 15:31:20 by vphongph          #+#    #+#             */
+/*   Updated: 2018/11/13 16:24:49 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+char*	rework_str(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\t')
+			str[i] = ' ';
+		i++;
+	}
+	i--;
+	while (str[i] == ' ')
+	{
+		str[i] = '\0';
+		i--;
+	}
+	while (*str == ' ')
+		str++;
+		
+	return (str);
+}
+
+int		main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		write (1, "\n", 1);
+		return (0);
+	}
+
+	printf("\nav = %s", av[1]);
+	printf("\nrework_str(av) = %s", rework_str(av[1]));
+	printf("%d\n", '\f');
+	return (0);
+}
