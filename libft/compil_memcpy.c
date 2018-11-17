497bcdeb075e3426434bb79be89d2bec681a6fe3
vphongph <vphongph@student.42.fr>
2018-11-18 00:46:21 +0100

    update libft

---
+++ libft/compil_memcpy.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   compil_memcpy.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/11 19:55:25 by vphongph          #+#    #+#             */
+/*   Updated: 2018/11/18 00:43:13 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+
+
+
+int		main(void)
+{
+
+	int	tabsrc[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	int	tabdest[20] = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+
+	int	tabsrc2[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	int	tabdest2[20] = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+
+	int i;
+	size_t n = 1000000000;
+
+	i = 0;
+	while (i < 20)
+	{
+		printf("%d", tabsrc[i++]);
+		fflush(stdout);
+		write(1, " ", 1);
+	}
+	fflush(stdout);
+	write(1, "\nSRC\n", 5);
+
+	i = 0;
+	while (i < 20)
+	{
+		printf("%d", tabsrc2[i++]);
+		fflush(stdout);
+		write(1, " ", 1);
+	}
+	fflush(stdout);
+	write(1, "\nSRC2\n", 6);
+
+	i = 0;
+	while (i < 20)
+	{
+		printf("%d", tabdest[i++]);
+		fflush(stdout);
+		write(1, " ", 1);
+	}
+	fflush(stdout);
+	write(1, "\nDEST\n", 6);
+
+	i = 0;
+		while (i < 20)
+	{
+		printf("%d", tabdest2[i++]);
+		fflush(stdout);
+		write(1, " ", 1);
+	}
+	fflush(stdout);
+	write(1, "\nDEST2\n", 7);
+
+	memcpy(tabdest, tabsrc, n);
+	ft_memcpy(tabdest2, tabsrc2, n);
+
+
+	i = 0;
+	while (i < 20)
+	{
+		printf("%d", tabdest[i++]);
+		fflush(stdout);
+		write(1, " ", 1);
+	}
+	fflush(stdout);
+	write(1, "\nDEST\n", 6);
+
+	i = 0;
+		while (i < 20)
+	{
+		printf("%d", tabdest2[i++]);
+		fflush(stdout);
+		write(1, " ", 1);
+	}
+	fflush(stdout);
+	write(1, "\nDEST2\n", 7);
+
+	return (0);
+}
--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,24 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/16 13:26:35 by vphongph          #+#    #+#             */
-/*   Updated: 2018/11/18 00:33:18 by vphongph         ###   ########.fr       */
+/*   Created: 2018/11/17 23:54:41 by vphongph          #+#    #+#             */
+/*   Updated: 2018/11/18 00:33:26 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# define BUF_SIZE 128
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
+#include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t i;
 
-#endif
+	i = 0;	
+
+	while (i < n)
+	{
+		((char *)dst)[i] = ((char *)src)[i];
+		i++;
+	}
+
+	return(dst);
+}
