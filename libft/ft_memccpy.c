b62987011f194d320eb1d7413ac830cac099b17b
vphongph <vphongph@student.42.fr>
2018-11-18 03:03:01 +0100

    update libft

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 23:54:41 by vphongph          #+#    #+#             */
-/*   Updated: 2018/11/18 02:48:00 by vphongph         ###   ########.fr       */
+/*   Updated: 2018/11/18 03:02:15 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{	
-		if (((const char *)src)[i] == (unsigned char) c)
+		if (((const unsigned char *)src)[i] == (unsigned char) c)
 		{
-			((char *)dst)[i] = ((const char *)src)[i];
+			((unsigned char *)dst)[i] = ((const unsigned char *)src)[i];
 			i++;
-			return (dst);
+			return (dst + i);
 		}
 		else 
-			((char *)dst)[i] = ((const char *)src)[i];
+			((unsigned char *)dst)[i] = ((const unsigned char *)src)[i];
 			i++;
 	}
 	return (NULL);
