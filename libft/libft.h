d666ffac447eda3f5ff341e4e8c34d747166cd47
vphongph <vphongph@student.42.fr>
2018-11-18 01:40:14 +0100

    update libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,15 +6,13 @@
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 13:26:35 by vphongph          #+#    #+#             */
-/*   Updated: 2018/11/18 00:33:18 by vphongph         ###   ########.fr       */
+/*   Updated: 2018/11/18 01:30:15 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # define BUF_SIZE 128
-# include <unistd.h>
-# include <stdlib.h>
 # include <string.h>
 
 void	*ft_memset(void *b, int c, size_t len);
