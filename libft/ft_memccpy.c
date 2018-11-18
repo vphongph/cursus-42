181e1a51658356e148a4b9d4bdee037d748f73b1
vphongph <vphongph@student.42.fr>
2018-11-18 01:56:35 +0100

    update libft

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -1,26 +1,11 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/17 23:54:41 by vphongph          #+#    #+#             */
-/*   Updated: 2018/11/18 01:55:27 by vphongph         ###   ########.fr       */
+/*   Created: 2018/11/18 01:49:59 by vphongph          #+#    #+#             */
+/*   Updated: 2018/11/18 01:55:08 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t i;
-
-	i = 0;
-	while (i < n)
-	{
-		((char *)dst)[i] = ((char *)src)[i];
-		i++;
-	}
-	return (dst);
-}
