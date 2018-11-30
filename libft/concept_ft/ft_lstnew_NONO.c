ac472117de2b4a68eb8d5264c771f445d60e8cf8
vphongph <vphongph@student.42.fr>
2018-11-30 17:59:59 +0100

    update libft lstnew avec nono

--- libft/concept_ft/ft_lstnew_NONO.c
+++ libft/concept_ft/ft_lstnew_NONO.c
@@ -6,7 +6,7 @@
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 00:14:23 by vphongph          #+#    #+#             */
-/*   Updated: 2018/11/30 17:59:28 by vphongph         ###   ########.fr       */
+/*   Updated: 2018/11/30 17:43:47 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,29 @@ t_list 	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list *newlst;
 
-	if (!(newlst = ft_memalloc(sizeof(t_list))) || !content)
+	if (!(newlst = ft_memalloc(sizeof(t_list))))
 		return (NULL);
 	newlst->content = (void *)content;
-	newlst->content_size = sizeof(content_size);
+	(*newlst).content
+	(int)(*((char*)(newlst) + 8))
+	               ------
+	               t_list *0
+	        ---------------
+	             char *
+	       ----------------------
+	       		char * [lstnew + 8]
+	     ------------------------
+	     		char 
+	    ------------------------------
+	    			int
+
+	newlst->content_size = content_size;
 	newlst->next = NULL;
 
 	return (newlst);
 }
+
+						 |
+                         v
+[ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]
+content                 |   content_size        |           next
