fc9d5564444237afbfa8cf0ca4dc767fbefcebac
vphongph <vphongph@student.42.fr>
2018-12-04 20:40:56 +0100

    update libft lstmap

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,11 +6,12 @@
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 00:20:16 by vphongph          #+#    #+#             */
-/*   Updated: 2018/12/04 19:27:33 by vphongph         ###   ########.fr       */
+/*   Updated: 2018/12/04 20:16:55 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
@@ -22,6 +23,13 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 		return (NULL);
 	while (lst)
 	{
+		if (!(newlst = ft_memalloc(sizeof(t_list))))
+			return (NULL);
+		if (!(newlst->content = ft_memalloc(lst->content_size)))
+		{
+			free(newlst);
+			return (NULL);
+		}
 		add = f(lst);
 		ft_lstaddend(&newlst, add);
 		lst = lst->next;
