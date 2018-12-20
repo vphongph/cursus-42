063cf5e7fbe8407d1b0e502668e5be669bd8bf57
vphongph <vphongph@student.42.fr>
2018-12-20 20:30:29 +0100

    update GNL update sanitize

--- get_next_line/test_random.c
+++ get_next_line/test_random.c
@@ -6,7 +6,7 @@
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 01:35:06 by vphongph          #+#    #+#             */
-/*   Updated: 2018/12/20 20:11:28 by vphongph         ###   ########.fr       */
+/*   Updated: 2018/12/20 20:08:27 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,28 @@ void 	test_leaks(void)
 
 int		main(void)
 {
-	// int i = -1;
-	// // char *str;
-	// // char *str1 = NULL;
-	// char str1[26] = "Hello je suis un Pingouin";
-	// char str1_1[41] = ALLIANCE"Vive l'Alliance !\n"RESET;
-	// // char *str2 = NULL;
-	// char str2[50] = " qui respire par le cul, je m'assieds, je meurs.\n";
-	// // static char str2[100];
-	// char *str3 = NULL;
-	// char str4[26];
-	// str4[25] = '!';
+	int i = -1;
+	// char *str;
+	// char *str1 = NULL;
+	char str1[26] = "Hello je suis un Pingouin";
+	char str1_1[41] = ALLIANCE"Vive l'Alliance !\n"RESET;
+	// char *str2 = NULL;
+	char str2[50] = " qui respire par le cul, je m'assieds, je meurs.\n";
+	// static char str2[100];
+	char *str3 = NULL;
+	char str4[26];
+	str4[25] = '!';
 
-	// ft_memcpy_v2(str4, str1, ft_strlen_v2(str1) + 1);
-	// printf("%s\n", str4);
+	ft_memcpy_v2(str4, str1, ft_strlen_v2(str1) + 1);
+	printf("%s\n", str4);
 
 	// ft_bzero_v2(str4, 1000000);
 
 	// test_leaks();
 
-	// str3 = ft_memdup(str1_1,ft_strlen_v2(str1_1) + 1);
-	// printf("%s\n", str3);
-	// free(str3);
+	str3 = ft_memdup(str1_1,ft_strlen_v2(str1_1) + 1);
+	printf("%s\n", str3);
+	free(str3);
 
 
 	// str3 = ft_memjoin(str1, str2, ft_strlen_v2(str1), ft_strlen_v2(str2));
@@ -54,25 +54,21 @@ int		main(void)
 	// free(str3);
 
 	ft_memalloc(1000);
-	ft_memalloc(1000);
-	ft_memalloc(1000);
-	ft_memalloc(1000);
-
 	// free(str3);
 
-	// char *str5 = ft_memalloc(25 + 1);
-	// while (str1[++i])
-	// 	str5[i] = str1[i];
-	// str5 = ft_memjoinfree_l(str5, str2, 25, 49 + 1);
-	// printf("%s\n", str5);
+	char *str5 = ft_memalloc(25 + 1);
+	while (str1[++i])
+		str5[i] = str1[i];
+	str5 = ft_memjoinfree_l(str5, str2, 25, 49 + 1);
+	printf("%s\n", str5);
 
 	// free(str5);
 
-	// str3 = ft_strsub_v2(str5, 6	, 66);
+	str3 = ft_strsub_v2(str5, 6	, 66);
 
-	// free(str5);
-	// printf("%s\n", str3);
-	// free(str3);
+	free(str5);
+	printf("%s\n", str3);
+	free(str3);
 
 
 	// printf("%d\n", ft_putstr_fd_v2(str1, 1));
