de4c7fb1d2b4955094ab5371be2f1cffd3cb8e8e
vphongph <vphongph@student.42.fr>
2019-01-03 01:59:54 +0100

    nothing

---
+++ get_next_line/test_strlen.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_strlen.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/17 02:07:42 by vphongph          #+#    #+#             */
+/*   Updated: 2019/01/03 01:09:57 by vphongph         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <unistd.h>
+
+int		main(void)
+{
+	// char *str = NULL;
+	char str[301] = "2345678wro;iewroiew*&^&^%&*^$%&67+)_080w7ekljhvkfjhlivudfhsl35fg43654vboh^&%^%$&^$fgjhgweury123456782345678wro;iewroiew*&^&^%&*^$%&67+)_080w7ekljhvkfjhlivudfhsl35fg43654vboh^&%^%$&^$fgjhgweury123456782345678wro;iewroiew*&^&^%&*^$%&67+)_080w7ekljhvkfjhlivudfhsl35fg43654vboh^&%^%$&^$fgjhgweury12345678";
+	// char str[7] = {-128, -127, -1, -10, 5, 1, 0};
+
+	int i = 10000000;
+
+	while (i--)
+	{
+		ft_strlen_v2(str);
+	}
+
+	printf("      str = %s\n", str);
+	printf("   strlen = %lu\n", strlen(str));
+	// printf("   strlen = %lu\n", ft_strlen(str));
+	printf("strlen v2 = %d\n", ft_strlen_v2(str));
+
+	return (0);
+}
