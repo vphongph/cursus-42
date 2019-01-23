ee72a6872e81363c0913c694eddc22b9b78e7b57
vphongph <vphongph@student.42.fr>
2019-01-24 00:36:47 +0100

    test malloc

--- test_malloc.c
+++ test_malloc.c
@@ -6,7 +6,7 @@
 /*   By: vphongph <vphongph@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 20:35:26 by vphongph          #+#    #+#             */
-/*   Updated: 2019/01/23 20:47:29 by vphongph         ###   ########.fr       */
+/*   Updated: 2019/01/24 00:25:34 by vphongph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,46 @@
 #include <malloc/malloc.h>
 #include <stdio.h>
 
-int		main(void)
+	int		main(void)
 {
-	size_t i = ((4096 * 2) + (512 * 3) + (17 * 16));
+	size_t i = 4096 * 32 - 1;
+	size_t j = 0;
 	char *str = malloc(i);
 
-	printf("%lu\n", i);
-	printf("%lu\n", malloc_size(str));
-	printf("%lu\n", malloc_good_size(i));
+	printf("i = %lu\n", i);
+
+
+	if (i / 4096 >= 32)
+	{
+		j = (i % 4096 ? 1 : 0);
+		printf("malloc good = 4096 * %lu = %lu\n",
+			i / 4096 + j,
+			4096 * (i / 4096 + j));
+	}
+	else if (i / 4096)
+	{
+		j = (i % 4096 % 512 ? 1 : 0);
+		printf("malloc good = 4096 * %lu + 512 * %lu = %lu\n",
+			i / 4096,
+			i % 4096 / 512 + j,
+			4096 * (i / 4096) + 512 * (i % 4096 / 512 + j));
+	}
+	else if (i / 16 >= 64)
+	{
+		j = (i % 512 ? 1 : 0);
+		printf("malloc good = 512 * %lu = %lu\n",
+			i / 512 + j,
+			512 * (i / 512 + j));
+	}
+	else
+	{
+		j = (i % 16 ? 1 : 0);
+		i == 0 ? j = 1 : j;
+		printf("malloc good = 16 * %lu = %lu\n",
+			i / 16 + j,
+			16 * (i / 16 + j));
+	}
+	printf("malloc = %lu\n", malloc_size(str));
 
 	return (0);
 }
